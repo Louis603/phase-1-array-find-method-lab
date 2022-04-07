7c3a560d4a69ef667ba7de2b76f835ed2644da26
Louis Y <louisy603@hotmail.com>
2022-04-07 12:58:45 -0400

    commit tree 67fae73c5a131e45049d204688cc2855dc72cb31

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+
+  
+function superbowlWin(array) {
+    const answer = array.find(season => season.result === 'W')
+    if (answer == undefined){
+      return undefined
+    }
+
+    return answer.year;
+}
+
